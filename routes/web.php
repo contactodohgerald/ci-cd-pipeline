69ec928344ecc63b84ddf5ae425213540e28eeea
Gerald Odoh <contactodohgerald@gmail>
2023-07-03 13:05:36 +0100

    made newer changes to the route/web

--- routes/web.php
+++ routes/web.php
@@ -16,12 +16,6 @@ use Inertia\Inertia;
 |
 */
 
-// Route::get('/', function () {
-//     return Inertia::render('Welcome', [
-//         'canLogin' => Route::has('login'),
-//         'canRegister' => Route::has('register'),
-//     ]);
-// });
 Route::get('/', [PostController::class, 'index']);
 
 Route::get('/dashboard', [PostController::class, 'dashboard'])->middleware(['auth', 'verified'])->name('dashboard');
@@ -30,7 +24,6 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-
 
     Route::post('/post', [PostController::class, 'store'])->name('post.create');
     
